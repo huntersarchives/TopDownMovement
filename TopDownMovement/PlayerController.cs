16eeb5c3c0074484b0d3f1ccdfe9dcee9ee411b8
Hunter <62785848+huntersarchives@users.noreply.github.com>
2026-01-01 21:11:04 -0500

    Remove empty Start method in PlayerController
    Removed unused Start method from PlayerController.

--- TopDownMovement/PlayerController.cs
+++ TopDownMovement/PlayerController.cs
@@ -10,11 +10,6 @@ public class PlayerController : MonoBehaviour
 
     [SerializeField] private Rigidbody2D rb;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-    
-    }
 
     // Update is called once per frame
     void Update()
